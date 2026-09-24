03ec7cc92cbc39340540f374d0c97e6ea6b3a310
arkhan66648 <rmadskhanfirst4323@gmail.com>
2026-09-24 17:16:22 +0600

    Remove changefreq from sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -6,7 +6,6 @@ export const GET: APIRoute = () => {
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>${SITE.url}/</loc>
-    <changefreq>hourly</changefreq>
     <priority>1.0</priority>
   </url>
 </urlset>`;
